31fb8e0694d02eff14528256a6e0486c8fb51c70
ns3838 <72807027+ns3838@users.noreply.github.com>
2021-02-08 02:20:16 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-def on_forever():
-    pass
-forever(on_forever)
+while True:
+    print ("X:" + input.acceleration(Dimension.X) + "  Y:" + input.acceleration(Dimension.Y) + "  Z:" + input.acceleration(Dimension.Z))
+    if input.acceleration(Dimension.Z) > 25:
+        light.set_all(light.rgb(255,0,0))
+    else:
+        light.clear()
+
+
--- main.ts
+++ main.ts
@@ -1,3 +1,9 @@
-forever(function on_forever() {
+while (true) {
+    console.log("X:" + input.acceleration(Dimension.X) + "  Y:" + input.acceleration(Dimension.Y) + "  Z:" + input.acceleration(Dimension.Z))
+    if (input.acceleration(Dimension.Z) > 25) {
+        light.setAll(light.rgb(255, 0, 0))
+    } else {
+        light.clear()
+    }
     
-})
+}
