f1aff9bbff9dad6f4d8c6280fd4a3d838b0264e1
ns3838 <72807027+ns3838@users.noreply.github.com>
2021-02-09 00:14:40 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 while True:
     print ("X:" + input.acceleration(Dimension.X) + "  Y:" + input.acceleration(Dimension.Y) + "  Z:" + input.acceleration(Dimension.Z))
-    if input.acceleration(Dimension.Z) > 25:
-        light.set_all(light.rgb(255,0,0))
+    if input.acceleration(Dimension.X) > 15:
+        light.set_all(light.rgb(0,255,0))
     else:
-        light.clear()
+        light.set_all(light.rgb(255,0,0))
 
 
--- main.ts
+++ main.ts
@@ -1,9 +1,9 @@
 while (true) {
     console.log("X:" + input.acceleration(Dimension.X) + "  Y:" + input.acceleration(Dimension.Y) + "  Z:" + input.acceleration(Dimension.Z))
-    if (input.acceleration(Dimension.Z) > 25) {
-        light.setAll(light.rgb(255, 0, 0))
+    if (input.acceleration(Dimension.X) > 15) {
+        light.setAll(light.rgb(0, 255, 0))
     } else {
-        light.clear()
+        light.setAll(light.rgb(255, 0, 0))
     }
     
 }
